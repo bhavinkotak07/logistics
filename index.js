40b85519261b7e15cc7686a0d335c1616531447f
Bhavin Kotak <bhavinkotak07@gmail.com>
2017-12-10 19:26:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
-
+var port = process.env.PORT || 8080;
 app.get('/listOrders', function (req, res) {
    fs.readFile( __dirname + "/" + "orders.json", 'utf8', function (err, data) {
        console.log( data );
@@ -22,10 +22,10 @@ app.get('/orders/:email', function (req, res) {
       res.end( JSON.stringify(picked));
    });
 })
-var server = app.listen(8085, function () {
+var server = app.listen(port, function () {
 
   var host = server.address().address
-  var port = server.address().port
+  
 
   console.log("Example app listening at http://%s:%s", host, port)
 
